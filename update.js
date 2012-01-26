5220627a15432cab189b84564fffaf06799a6508
harald pichler <harald@the-develop.net>
2012-01-26 22:27:28 +0100

    add no network code

--- update.js
+++ update.js
@@ -1,5 +1,42 @@
 {
-	var serverip="192.168.3.101";
+	var serverip="192.168.1.101";
+    var online =  false;
+    
+	function check_network() {
+    	var timeOutInteger = null;
+    	var timeOutOccured = false;
+
+                var url = 'http://' + serverip + '/images/boxbg.gif';
+ 
+                    $.ajax({ type: "GET",
+                            data: "{}",
+                            url: url,
+                            cache: false,
+                            timeout: 10 * 1000,
+                            success:function(response)
+                            {
+                                if(!timeOutOccured){
+                                    clearTimeout(timeOutInteger);
+                                }
+                                online = true;
+                                //confirm('Connection type: \n ' + online);
+                                getenv();
+                                
+                            },
+                            error:function(xhr, textStatus, errorThrown) {
+                                if(!timeOutOccured){
+                                    clearTimeout(timeOutInteger);
+                                }
+                                online = false;
+                                confirm('Connection type: \n ' + online);  
+                            }
+                     });   	             
+	};
+	
+
+	$(document).bind("deviceready", function () {
+			alert("ready");
+	});	
 
 	$( document ).bind( "mobileinit", function() {
     	// Make your jQuery Mobile framework configuration changes here!
@@ -9,14 +46,15 @@
 
  	$('#home').live('pageinit', function(event) {
 		console.log('pageinit triggered.');
-      		//alert("This happens");
-		//Assign default value to form field #1
+		//disable cache
                 $.ajaxSetup({ cache: false });
 		// server ip
-                $('#ipname').val(serverip);
+                $('#ipname').val(serverip);  
+		// check online
+		check_network();
 
 		var reload = setInterval(getenv,20000);
-		getenv();
+		//getenv();
 		// an anchor element gets clicked
                 $('#form1').submit(function() { 
 		  serverip = $('#ipname').val();
@@ -24,6 +62,7 @@
 
 		$("#button").click( function() {
                         serverip = $('#ipname').val();
+                        check_network();
 		});
 
 		$('#slider1').change(function() {
@@ -112,8 +151,11 @@
 
 	function getenv(){
 		//Assign default value to form field #1
+        if(online) {
+  		// make a request
+  		//  alert('Connection type: ' + "online");
 
-                $.getJSON('http://' + serverip + '/cgi-bin/uenv.cgi',
+        $.getJSON('http://' + serverip + '/cgi-bin/uenv.cgi',
 			function(data){
 			var myswitch = $("select#slider1");
 			myswitch[0].selectedIndex = (data.relay1 == 1);
@@ -149,6 +191,10 @@
 			$('#stext7').text(data.relname7);
 			$('#stext8').text(data.relname8);
 		});
+				} else {
+  		// load from localStorage
+		  alert('Connection type: ' + "offline");
+		};
 	};
 	// this function parses the fetched data into the 'info' paragraph
 	function parseInfo(data)
